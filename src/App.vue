7e34f853115c2858a6e1e2a2ad33b1a7a9624e2d
Adriano de Moura Leite <adriano.lols@gmail.com>
2021-10-22 11:56:27 -0300

    basic changes to project structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/chat">Chat</router-link>
   </div>
   <router-view/>
 </template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style>
 #app {
---
+++ src/views/Chat.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <w-button icon="f"/>
+    <w-plate/>
+  </div>
+</template>
+
+<script>
+import WButton from '@/widgets/Button'
+import WPlate from '@/widgets/Plate'
+export default {
+  name: 'Chat',
+  components: { WPlate, WButton }
+}
+</script>
+
+<style scoped>
+
+</style>
