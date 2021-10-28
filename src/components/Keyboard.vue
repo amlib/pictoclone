7641398f054d4fee7d4ed4648515b195dad4724a
Adriano de Moura Leite <adriano.lols@gmail.com>
2021-10-28 16:59:37 -0300

    Move constant keyboard definitions to its own file

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -32,70 +32,7 @@
 import WPlate from '@/widgets/Plate'
 import WButton from '@/widgets/Button'
 import { throttle } from 'lodash'
-
-const layouts = {
-  romaji: {
-    sections: [
-      {
-        class: 'romaji-row1',
-        keyClass: 'key-14px',
-        keys: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
-        shiftKeys: ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']
-      },
-      {
-        class: 'romaji-row2',
-        keyClass: 'key-15px',
-        keys: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'backspace'],
-        shiftKeys: ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'backspace']
-      },
-      {
-        class: 'romaji-row2',
-        keyClass: 'key-15px',
-        keys: ['caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'enter'],
-        shiftKeys: ['caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'enter']
-      },
-      {
-        class: 'romaji-row4',
-        keyClass: 'key-15px',
-        keys: ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'],
-        shiftKeys: ['shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?']
-      },
-      {
-        class: 'romaji-row5',
-        keyClass: 'key-14px',
-        keys: [';', '\'', 'space', '[', ']'],
-        shiftKeys: [':', '"', 'space', '{', '}']
-      }
-    ]
-  }
-}
-
-const uniqueKeyTile = {
-  backspace: true,
-  enter: true,
-  caps: true,
-  shift: true,
-  space: true
-}
-
-const uniqueKeyIcon = {
-  backspace: true,
-  enter: true,
-  caps: true,
-  shift: true,
-  space: true
-}
-
-const uniqueKeyIconMargin = {
-  backspace: [1, 1, 1, 1],
-  enter: [1, 1, 1, 1],
-  caps: [1, 1, 1, 1],
-  shift: [1, 1, 1, 1],
-  space: [1, 29, 1, 29]
-}
-
-const uniqueKeyClass = {
-}
+import { layouts, uniqueKeyTile, uniqueKeyIcon, uniqueKeyIconMargin, uniqueKeyClass } from '@/js/Keyboard'
 
 export default {
   name: 'Keyboard',
